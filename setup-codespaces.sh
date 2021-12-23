35023ac86df9d3ae7cd5ec7143d3c6fd6de6778d
Jurre Stender <jurre@github.com>
2021-12-23 16:06:39 +0100

    Add fuse for nvim

--- setup-codespaces.sh
+++ setup-codespaces.sh
@@ -5,18 +5,28 @@ rm -f "$HOME/.zshrc"
 rm -f "$HOME/.gitconfig"
 
 PACKAGES_NEEDED="\
+  tmux \
+  grc \
   shellcheck \
   zsh-autosuggestions \
+  fuse \
   rubocop \
+  fzf \
+  fasd \
   ruby-dev \
-  silversearcher-ag"
+  silversearcher-ag \
+  bat \
+  libfuse2"
 
 if ! dpkg -s "${PACKAGES_NEEDED}" > /dev/null 2>&1; then
-  sudo apt update --fix-missing
-  sudo apt -y -q install ${PACKAGES_NEEDED} --fix-missing
+  sudo apt-get update --fix-missing
+  sudo apt-get -y -q install ${PACKAGES_NEEDED} --fix-missing
 fi
 
 # install latest neovim
+sudo modprobe fuse
+sudo groupadd fuse
+sudo usermod -a -G fuse "$(whoami)"
 wget https://github.com/github/copilot.vim/releases/download/neovim-nightlies/appimage.zip
 unzip appimage.zip
 sudo chmod u+x nvim.appimage
