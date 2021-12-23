3312adb4136847a62b825c730e8a1cd6e6c4ba5f
Jurre Stender <jurre@github.com>
2021-12-23 15:55:43 +0100

    fewer packages

--- setup-codespaces.sh
+++ setup-codespaces.sh
@@ -5,19 +5,11 @@ rm -f "$HOME/.zshrc"
 rm -f "$HOME/.gitconfig"
 
 PACKAGES_NEEDED="\
-  tmux \
-  grc \
   shellcheck \
   zsh-autosuggestions \
-  fuse \
-  npm \
   rubocop \
-  fzf \
-  fasd \
   ruby-dev \
-  silversearcher-ag \
-  bat \
-  libfuse2"
+  silversearcher-ag"
 
 if ! dpkg -s "${PACKAGES_NEEDED}" > /dev/null 2>&1; then
   sudo apt-get update --fix-missing
@@ -25,9 +17,6 @@ if ! dpkg -s "${PACKAGES_NEEDED}" > /dev/null 2>&1; then
 fi
 
 # install latest neovim
-sudo modprobe fuse
-sudo groupadd fuse
-sudo usermod -a -G fuse "$(whoami)"
 wget https://github.com/github/copilot.vim/releases/download/neovim-nightlies/appimage.zip
 unzip appimage.zip
 sudo chmod u+x nvim.appimage
