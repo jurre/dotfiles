6ec76b40e6dfd6ed2df3f85439d5b8c2081d9725
Jurre Stender <jurre@github.com>
2023-05-02 15:43:52 +0200

    Install a recent version of node for copilot

--- setup-codespaces.sh
+++ setup-codespaces.sh
@@ -37,3 +37,4 @@ git clone https://github.com/nodenv/nodenv.git ~/.nodenv
 ~/.nodenv/bin/nodenv init
 mkdir -p "$(nodenv root)"/plugins
 git clone https://github.com/nodenv/node-build.git "$(nodenv root)"/plugins/node-build
+nodenv install 16.20.0
